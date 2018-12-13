bd68d6e3db16be37a1ecf34c9a03d638a7e099b3
bing.zhub <bing.zhub@gmail.com>
2018-12-13 11:28:48 +0800

    retrain the mode

--- nlu_model.py
+++ nlu_model.py
@@ -7,10 +7,10 @@ def train_nlu(data, configs, model_dir):
   training_data = load_data(data)
   trainer = Trainer(config.load(configs))
   trainer.train(training_data)
-  model_directory = trainer.persist(model_dir, fixed_model_name = 'testnlu')
+  model_directory = trainer.persist(model_dir, fixed_model_name = 'criminalnlu')
 
 def run_nlu():
-  interpreter = Interpreter.load('./models/nlu/default/testnlu/')
+  interpreter = Interpreter.load('./models/nlu/default/criminalnlu/')
   print(interpreter.parse("你好"))
 
 if __name__ == '__main__':
